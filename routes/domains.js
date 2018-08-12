7c336ff807f955e228fb005b2ca737a686410f51
Eric Fossas <serpicofossas@gmail.com>
2018-08-12 11:45:56 -0600

    Add instructions

--- routes/domains.js
+++ routes/domains.js
@@ -4,7 +4,9 @@ exports.route = function(request,response) {
   const domains = request.app.get('domains');
   
   let links = [];
-  links.push(`<h2>Welcome To RESTful CRUD!</h2><p>Select A Domain Below To Start Creating Records.</p>
+  links.push(`<h2>Welcome To RESTful CRUD!</h2>
+    <p>Select A Domain Below To Start Creating Records.</p>
+    <p>To Search For Records, Go To: ${host}/<b style="color:red">DOMAIN</b>/<b style="color:blue">FIELD</b>/<b style="color:green">VALUE</b>.<br><b style="color:green">VALUE</b> can be a regex expression or use an asterisk * to get all records.</p>
     <hr>
     <div class="height-20"></div>
     <ul>`);
